48634f4b3efcf7ad1265b7c209b6a3419067a493
Samya brata Maji <samyabratamaji334@gmail.com>
2023-04-09 11:08:25 +0530

    removed unnecessary fetches

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,64 +7,58 @@ import { MyContext } from "../context/contextProvider";
 function Home() {
   const [search, setsearch] = useContext(MyContext);
   const [movies, setmovies] = useState([
-    {id:"",
-      name: "",
-      summary: "",
-      image: "",
-    },
+    { id: "", name: "", summary: "", image: "" },
   ]);
-  console.log(search);
 
   useEffect(() => {
     setmovies([]);
-    async function searchMovie() {
-      const result = await fetch(
-        `https://api.tvmaze.com/search/shows?q=${search}`
-      )
-        .then((res) => {
-          return res.json();
-        })
-        .then((response) => {
-          console.log(response);
 
-          response.map((show) => {
-            console.log(show);
-            setmovies([
-              ...movies,
-              {
-                id:show.show.id,
-                name: show.show.name,
-                summary: show.show.summary,
-                image: show.show.image.medium,
-              },
-            ]);
+    async function searchMovie(searchState) {
+      if (searchState) {
+        await fetch(`https://api.tvmaze.com/search/shows?q=${search}`)
+          .then((res) => {
+            return res.json();
+          })
+          .then((response) => {
+            console.log(response);
+
+            setmovies(() => {
+              return response.map((show) => {
+                return {
+                  id: show.show.id,
+                  name: show.show.name,
+                  summary: show.show.summary,
+                  image: show.show.image.medium,
+                };
+              });
+            });
           });
-        });
+      }
     }
-    searchMovie();
+    searchMovie(search);
   }, [search]);
 
   return (
     <div>
+      <div className="row">
+        <Navbar />
+      </div>
+      <div className="row container" style={{ margin: "70px 20px 10px 20px" }}>
         <div className="row">
-          <Navbar />
+          {movies.map((mov, index) => {
+            return (
+              <div key={index} className="col">
+                <MoviesCard
+                  id={mov.id}
+                  name={mov.name}
+                  img={mov.image}
+                  summary={mov.summary}
+                />
+              </div>
+            );
+          })}
         </div>
-        <div className="row container" style={{ margin: "70px 20px 10px 20px" }}>
-          <div className="row">
-            {movies.map((mov) => {
-              return (
-                <div className="col">
-                  <MoviesCard
-                    id={mov.id}
-                    name={mov.name}
-                    img={mov.image}
-                    summary={mov.summary}
-                  />
-                </div>
-              );
-            })}
-          </div>
-        </div>
+      </div>
     </div>
   );
 }
